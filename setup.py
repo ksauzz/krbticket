5f2d3771051ffb0018fe58e6d6116e68132005de
Kazuhiro Suzuki <ksauzzmsg@gmail.com>
2018-10-25 12:24:25 +0900

    bump version to 0.0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="krbticket",
-    version="0.0.1.2",
+    version="0.0.1.3",
     author="Kazuhiro Suzuki",
     author_email="ksauzzmsg@gmail.com",
     description="Kerberos Ticket Manager",
