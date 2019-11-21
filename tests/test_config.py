68b9fed320d505bdbcf23aabe4e696268571547b
Kazuhiro Suzuki <ksauzzmsg@gmail.com>
2019-11-22 00:04:52 +0900

    add tests

---
+++ tests/test_config.py
@@ -0,0 +1,9 @@
+from krbticket import KrbConfig
+import os
+
+def test_ccache_name():
+    assert KrbConfig().ccache_name == '/tmp/krb5cc_{}'.format(os.getuid())
+    assert KrbConfig(ccache_name="/tmp/hoge").ccache_name == '/tmp/hoge'
+    os.environ['KRB5CCNAME'] = '/tmp/env_krb5cc'
+    assert KrbConfig().ccache_name == '/tmp/env_krb5cc'
+    del os.environ['KRB5CCNAME']
