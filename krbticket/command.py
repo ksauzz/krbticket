10a27c27047f88a2567d7bb174ca24bb05e9f029
Kazuhiro Suzuki <ksauzzmsg@gmail.com>
2019-11-22 00:24:13 +0900

    do not retry if FileNotFoundError raise.

--- krbticket/command.py
+++ krbticket/command.py
@@ -54,7 +54,16 @@ class KrbCommand():
     @staticmethod
     def _call(config, commands):
 
-        @retry(**config.retry_options)
+
+        def error_on_retry(exception):
+            # will not retry if command is not found.
+            if type(exception) == FileNotFoundError:
+                raise exception
+
+            return True
+
+        retry_options = { **config.retry_options, **{ 'retry_on_exception': error_on_retry }}
+        @retry(**retry_options)
         def retriable_call():
             logging.debug("Executing {}".format(" ".join(commands)))
             custom_env = os.environ.copy()
